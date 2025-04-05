921d3752fcb33902f7cf9d74141409688db3fc69
SaChan <vltpcks@gmail.com>
2025-04-05 13:07:36 +0900

    메인 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WhiteBoard from "./Whiteboard";
+import Main from "./main";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Main />} />
         <Route path="/whiteboard" element={<WhiteBoard />} />
       </Routes>
     </BrowserRouter>
