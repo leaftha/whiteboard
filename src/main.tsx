b1a778c41eb20ba0ac297209eec27aa7ba3b82b8
SaChan <vltpcks@gmail.com>
2025-04-05 18:48:38 +0900

    화이트 보드 방 나누기

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,54 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, doc, addDoc, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
 import { v4 as uuid } from "uuid";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Main = () => {
-  console.log(process.env.REACT_APP_FireBase_apiKey);
+  const [prjList, setPrjList] = useState<{ id: String; roomeId?: String }[]>(
+    []
+  );
+
+  useEffect(() => {
+    const ReadprjList = async () => {
+      const prjListRef = collection(db, "users", "vltpcks", "projectList");
+      const querySnapshot = await getDocs(prjListRef);
+
+      const list = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPrjList(list);
+    };
+
+    ReadprjList();
+  }, []);
+
   const newProject = async () => {
     const id = uuid();
     try {
       const docRef = await addDoc(collection(db, "project"), {
-        roomId: id,
+        projectName: "",
+        roomeId: id,
       });
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
-
+  console.log(prjList);
   return (
     <div>
-      <h1>Mainb</h1>
+      <h1>Main</h1>
       <button onClick={newProject}>새 프로젝트 생성</button>
+      <ul>
+        {prjList.map((prj) => (
+          <li key={String(prj.id)}>
+            <Link to="/whiteboard" state={{ roomeId: prj.roomeId }}>
+              {prj.id}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
