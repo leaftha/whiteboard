156bf5da2fc3d0ab83a7e13db4e0a93c8d56a24b
SaChan <vltpcks@gmail.com>
2025-04-05 14:01:27 +0900

    firebase 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
+import { v4 as uuid } from "uuid";
+
 const Main = () => {
+  console.log(process.env.REACT_APP_FireBase_apiKey);
+  const newProject = async () => {
+    const id = uuid();
+    try {
+      const docRef = await addDoc(collection(db, "project"), {
+        roomId: id,
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <div>
       <h1>Mainb</h1>
+      <button onClick={newProject}>새 프로젝트 생성</button>
     </div>
   );
 };
