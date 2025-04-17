f2947e7235e8852c80dc6cd06b4705dca7aebd5d
SaChan <vltpcks@gmail.com>
2025-04-17 22:03:02 +0900

    화이트 보드 이미지 기능 추가

--- server/assets.ts
+++ server/assets.ts
@@ -1,15 +1,44 @@
-import { mkdir, readFile, writeFile } from 'fs/promises'
-import { join, resolve } from 'path'
-import { Readable } from 'stream'
+import { mkdir, readFile, writeFile } from "fs/promises";
+import { join, resolve } from "path";
+import { Readable } from "stream";
+import * as admin from "firebase-admin";
 
-// We are just using the filesystem to store assets
-const DIR = resolve('./.assets')
+// Firebase Admin SDK 초기화
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      require("../whiteboard-374db-firebase-adminsdk-fbsvc-33d66f4d2c.json")
+    ),
+    storageBucket: "whiteboard-374db.firebasestorage.app",
+  });
+}
+
+const bucket = admin.storage().bucket();
 
 export async function storeAsset(id: string, stream: Readable) {
-	await mkdir(DIR, { recursive: true })
-	await writeFile(join(DIR, id), stream)
+  const file = bucket.file(`img/${id}`);
+  const writeStream = file.createWriteStream({
+    resumable: false,
+    contentType: "auto",
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    stream.pipe(writeStream).on("finish", resolve).on("error", reject);
+  });
 }
 
 export async function loadAsset(id: string) {
-	return await readFile(join(DIR, id))
+  const file = bucket.file(`img/${id}`);
+  const [exists] = await file.exists();
+  if (!exists) throw new Error("File not found");
+
+  const [buffer] = await file.download();
+  return buffer;
+}
+// assets.ts
+export async function deleteAsset(id: string) {
+  const file = bucket.file(id);
+  const [exists] = await file.exists();
+  if (!exists) throw new Error("File not found");
+  await file.delete();
 }
