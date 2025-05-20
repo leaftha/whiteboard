1ae47a62a566f9523de7cb3a5ac106bc99e47d86
SaChan <vltpcks@gmail.com>
2025-05-20 20:45:09 +0900

    메인페이지 디지인

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@
 import React from "react";
 import Router from "./Router";
 import { AuthProvider } from "./context/AuthContext";
-import Nav from "./components/nav";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Nav />
         <Router />
       </AuthProvider>
     </BrowserRouter>
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,10 +1,32 @@
-import { useAuth } from "../context/AuthContext";
-import AddProject from "./Addproject";
+import { Link } from "react-router-dom";
+import style from "./main.module.css";
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main</h1>
+    <div className={style.main}>
+      <div className={style.titleComponent}>
+        <div className={style.titleDescription}>
+          <h1 className={style.titleWord}>
+            팀 프로젝트 협업을 <br />
+            위한 길
+          </h1>
+          <p>답답한 프로그램에서 벗어나 웹에서의 회의</p>
+          <p>화상만이 아닌 협업 화이트 보드를 활용한 회의</p>
+          <div className={style.logComponent}>
+            <Link className={style.login} to="/login">
+              로그인
+            </Link>
+            <Link className={style.signup} to="/signup">
+              회원가입
+            </Link>
+          </div>
+        </div>
+        <img className={style.titleImg} src="image/main1.jpg" alt="main img" />
+      </div>
+
+      <div>
+        <h1>설명</h1>
+      </div>
     </div>
   );
 };
