8348ad2ad58cbc8f6d6151ced528393f891f7a33
SaChan <vltpcks@gmail.com>
2025-05-20 21:29:52 +0900

    로그아웃 버튼 추가

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import style from "./main.module.css";
+import { useAuth } from "../context/AuthContext";
+import { auth } from "../firebase";
 
 const Main = () => {
+  const { currentUser, loading } = useAuth();
+  const navigate = useNavigate();
+
+  const onLogOut = () => {
+    auth.signOut();
+    navigate("/");
+  };
+  console.log(currentUser);
   return (
     <div className={style.main}>
       <div className={style.titleComponent}>
@@ -13,12 +23,26 @@ const Main = () => {
           <p>답답한 프로그램에서 벗어나 웹에서의 회의</p>
           <p>화상만이 아닌 협업 화이트 보드를 활용한 회의</p>
           <div className={style.logComponent}>
-            <Link className={style.login} to="/login">
-              로그인
-            </Link>
-            <Link className={style.signup} to="/signup">
-              회원가입
-            </Link>
+            {!loading && (
+              <>
+                {currentUser === null ? (
+                  <>
+                    <Link className={style.login} to="/login">
+                      로그인
+                    </Link>
+                    <Link className={style.signup} to="/signup">
+                      회원가입
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <h1 onClick={onLogOut} className={style.logout}>
+                      로그아웃
+                    </h1>
+                  </>
+                )}
+              </>
+            )}
           </div>
         </div>
         <img className={style.titleImg} src="image/main1.jpg" alt="main img" />
