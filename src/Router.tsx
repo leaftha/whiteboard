0739c9ba0b18690a0798c14557b66cc4dfb6af34
SaChan <vltpcks@gmail.com>
2025-05-30 18:52:57 +0900

    라우터 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,12 +5,6 @@ import Project from "./components/Project";
 import Login from "./components/Login";
 import SignUp from "./components/signup";
 import MyProject from "./components/Myproject";
-
-import Main from "./components/main"; // 메인 페이지
-import Project from "./components/Project"; // 프로젝트 상세
-import Whiteboard from "./components/whiteboard"; // 화이트보드
-import Login from "./components/Login"; // 로그인
-import SignUp from "./components/signup"; // 회원가입
 import SchedulePage from "./schedule/SchedulePage"; // 일정 관리 페이지
 
 import "./App.css";
@@ -25,6 +19,7 @@ const AppRouter: React.FC = () => {
       <Route path="/whiteboard/:roomId" element={<WhiteBoard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/schedule" element={<SchedulePage />} />
     </Routes>
   );
 };
